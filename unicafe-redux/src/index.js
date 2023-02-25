ffee08f0bf986fc8ccd9a821a3cdeeee947b7f61
Red <crabmarket56@gmail.com>
2023-02-25 22:13:35 +0900

    Refactor index with redux provider

--- unicafe-redux/src/index.js
+++ unicafe-redux/src/index.js
@@ -1,7 +1,15 @@
+import React from 'react'
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './components/App'
 
+import counterReducer from './reducer';
 
+const store = createStore(counterReducer);
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
