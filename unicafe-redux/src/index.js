cfa2a655c6c703554a25e87fc1b0112f3ad2cbda
Red <crabmarket56@gmail.com>
2023-02-25 19:53:30 +0900

    implement counter functionality

--- unicafe-redux/src/index.js
+++ unicafe-redux/src/index.js
@@ -1,45 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import counterReducer from './reducer';
 
-import { createStore } from 'redux'
-import counterReducer from './reducer'
-const store = createStore(counterReducer)
+const store = createStore(counterReducer);
 
 const App = () => {
+  const [counts, setCounts] = useState(store.getState());
+
+  store.subscribe(() => {
+    setCounts(store.getState());
+  });
+
+  const handleGoodClick = () => {
+    store.dispatch({ type: 'GOOD' });
+  };
+
+  const handleOkClick = () => {
+    store.dispatch({ type: 'OK' });
+  };
+
+  const handleBadClick = () => {
+    store.dispatch({ type: 'BAD' });
+  };
+
+  const handleResetClick = () => {
+    store.dispatch({ type: 'ZERO' });
+  };
+
   return (
     <div>
       <div>
-        {store.getState()}
+        <h2>Feedback</h2>
+        <button onClick={handleGoodClick}>Good</button>
+        <button onClick={handleOkClick}>Ok</button>
+        <button onClick={handleBadClick}>Bad</button>
+        <button onClick={handleResetClick}>Reset</button>
       </div>
-      <button 
-        onClick={e => store.dispatch({ type: 'GOOD' })}
-      >
-        plus
-      </button>
-      <button
-        onClick={e => store.dispatch({ type: 'BAD' })}
-      >
-        minus
-      </button>
-      <button 
-        onClick={e => store.dispatch({ type: 'OK' })}
-      >
-        zero
-      </button>
-      <button 
-        onClick={e => store.dispatch({ type: 'ZERO' })}
-      >
-        zero
-      </button>
+      <div>
+        <h2>Statistics</h2>
+        <p>Good: {counts[0]}</p>
+        <p>Ok: {counts[1]}</p>
+        <p>Bad: {counts[2]}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const renderApp = () => {
-  root.render(<App />)
-}
-
-renderApp()
-store.subscribe(renderApp)
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
