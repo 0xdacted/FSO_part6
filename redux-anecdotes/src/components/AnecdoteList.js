8df40f3177c5a09dc1f6293851715ea2c26fd611
Red <crabmarket56@gmail.com>
2023-02-26 12:42:39 +0900

    Refactor anecdote list to component

---
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { voteForAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = ({ anecdotes }) => {
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(voteForAnecdote(id))
+  }
+
+  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
+
+  return (
+     <div>
+      {sortedAnecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList
