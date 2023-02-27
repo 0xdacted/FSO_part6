b3308d3cad0b8365fd6d6af179042f7228e89dee
Red <crabmarket56@gmail.com>
2023-02-27 14:15:50 +0900

    Set anecdote getting to app.js

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,10 +1,19 @@
+import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
+  
   const anecdotes = useSelector(state => state.anecdotes)
 
   return (
