ac37e01c969c1867205de2f1551ca19aa5fd81de
Red <crabmarket56@gmail.com>
2023-02-27 15:27:11 +0900

    Refactor store initialization using async action creators

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,6 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-
-const getId = () => (100000 * Math.random()).toFixed(0)
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -23,5 +22,11 @@ const anecdoteSlice = createSlice({
   }
 })
 
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
 export const { voteForAnecdote, addAnecdote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
