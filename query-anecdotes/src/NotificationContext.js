889a891d397add139708ea8b9b9a565a6cad12a9
Red <crabmarket56@gmail.com>
2023-02-28 13:32:07 +0900

    Create notification reducer and context provider

--- query-anecdotes/src/NotificationContext.js
+++ query-anecdotes/src/NotificationContext.js
@@ -1,4 +1,41 @@
-import { createContext } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
+
+const notificationReducer = (state, action) => {
+  switch(action.type) {
+    case 'SHOW_NOTIFICATION':
+      return {
+        message: action.payload,
+        show: true
+      }
+    case 'HIDE_NOTIFICATION':
+      return {
+        message: '',
+        show: false
+      }
+    default: 
+      return state;
+  }
+}
+
+
+export const NotificationContextProvider = (props) => {
+  const [notification, notificationDispatch] = useReducer(notificationReducer, { message: '', show: false })
+
+  useEffect(() => {
+    let notificationTimeout;
+    if (notification.show) {
+      notificationTimeout = setTimeout(() => {
+        notificationDispatch({ type: 'HIDE_NOTIFICATION' });
+      }, 5000)
+    }
+    return () => clearTimeout(notificationTimeout);
+  }, [notification.show])
+  return (
+    <NotificationContext.Provider value={ [notification, notificationDispatch] }>
+      {props.children}
+    </NotificationContext.Provider>
+  )
+}
 
 const NotificationContext = createContext();
 
