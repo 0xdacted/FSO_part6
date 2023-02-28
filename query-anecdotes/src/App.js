bd84e44b363fd7eeea2ad61f4c2d032aea7e4b15
Red <crabmarket56@gmail.com>
2023-02-28 14:21:09 +0900

    Added notification handling to vote

--- query-anecdotes/src/App.js
+++ query-anecdotes/src/App.js
@@ -2,13 +2,17 @@ import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import { useQuery, useQueryClient, useMutation} from 'react-query'
 import { getAnecdotes, updateAnecdote } from './requests'
+import { useNotificationDispatch } from './NotificationContext'
 
 const App = () => {
+  const dispatch = useNotificationDispatch()
 
   const queryClient = useQueryClient()
   
   const updateAnecdoteMutation = useMutation(updateAnecdote, {
     onSuccess: (updatedAnecdote) => {
+      dispatch({ type: 'SHOW_NOTIFICATION',
+                 payload: `Voted ${updatedAnecdote.content}` })
       queryClient.setQueryData('anecdotes', (oldAnecdotes) => {
         const newAnecdotes = oldAnecdotes.map((anecdote) => {
           if (anecdote.id === updatedAnecdote.id) {
@@ -24,6 +28,7 @@ const App = () => {
     console.log(anecdote)
     const updateAnecdote = { ...anecdote, votes: anecdote.votes + 1 }
     updateAnecdoteMutation.mutate(updateAnecdote)
+
   }
 
   const { isLoading, error, data: anecdotes} = useQuery(
